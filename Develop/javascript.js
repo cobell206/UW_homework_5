12a2e2e2f28881b5ea0d8e88acd85ff0f4c8aee8
Colin Bell <cobell206@gmail.com>
2021-01-28 23:08:36 -0800

    styling correct

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -3,3 +3,37 @@
 var current_date = luxon.DateTime.local()
 $("#currentDay").text(current_date)
 
+console.log(current_date.hour);
+
+var test = $(".input_box")
+
+console.log(test);
+console.log(test[0]);
+console.log(test[0].label);
+console.log(typeof(test[0]))
+console.log(Object.keys(test[0]))
+
+for(var i=0; i<test.length; i++) {
+    console.log(test[i].label)
+}
+
+
+// Array of input boxes
+// Loop through each box and assign appropriate class
+var input_boxes = ['11_textbox', '12_textbox', '13_textbox', '14_textbox', '15_textbox', '16_textbox']
+
+input_boxes.forEach(function(item, index) {
+    hour_ = $("#" + item).attr("data-hour")
+    console.log(hour_)
+    if (hour_ < current_date.hour) {
+        $("#" + item).addClass("past")
+    }
+    else if (hour_ == current_date.hour) {
+        $("#" + item).addClass("present")
+    }
+    else if (hour_ > current_date.hour) {
+        $("#" + item).addClass("future")
+    }
+    
+})
+
