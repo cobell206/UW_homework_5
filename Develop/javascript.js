c08826b1f6ba391c98f071a32c1118331c3b9bdb
Colin Bell <cobell206@gmail.com>
2021-01-28 23:35:19 -0800

    save function working

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,30 +1,51 @@
 
+// Array of input boxes
+var input_boxes = ['12_textbox', '13_textbox', '14_textbox', '15_textbox', '16_textbox']
+
+
+// Function to load data into array
+var calendar_data = [[], [], [], [], []]
+var load_data = function() {
+    if (localStorage.getItem("calendar_data")) {
+        console.log("present")
+        calendar_data = JSON.parse(localStorage.getItem("calendar_data"))
+        
+    }
+    else {
+        console.log("not here")
+        
+        localStorage.setItem("calendar_data", JSON.stringify(calendar_data))
+    }
+}
+
+// Function to loop through textboxes and fill in saved data
+var fill_in_saved_data = function() {
+    for (var i = 0; i < calendar_data.length; i++) {
+        $("#" + input_boxes[i]).val(calendar_data[i])
+        console.log(calendar_data[i]);
+    }
+}
+
+
+// Load data into array
+load_data()
+
+// Fill in data
+fill_in_saved_data()
+
+
 // Variable to store current time and fill in textbox
 var current_date = luxon.DateTime.local()
 $("#currentDay").text(current_date)
 
-console.log(current_date.hour);
 
 var test = $(".input_box")
 
-console.log(test);
-console.log(test[0]);
-console.log(test[0].label);
-console.log(typeof(test[0]))
-console.log(Object.keys(test[0]))
 
-for(var i=0; i<test.length; i++) {
-    console.log(test[i].label)
-}
-
-
-// Array of input boxes
 // Loop through each box and assign appropriate class
-var input_boxes = ['11_textbox', '12_textbox', '13_textbox', '14_textbox', '15_textbox', '16_textbox']
 
 input_boxes.forEach(function(item, index) {
     hour_ = $("#" + item).attr("data-hour")
-    console.log(hour_)
     if (hour_ < current_date.hour) {
         $("#" + item).addClass("past")
     }
@@ -37,3 +58,11 @@ input_boxes.forEach(function(item, index) {
     
 })
 
+// Press button to save
+$(".save_btn").on("click", function() {
+    hour_ = $(this).data("hour")
+    console.log(hour_)
+    calendar_data[hour_ - 12] = $("#" + hour_ + "_textbox").val()
+    console.log(calendar_data);
+    localStorage.setItem("calendar_data", JSON.stringify(calendar_data))
+})
